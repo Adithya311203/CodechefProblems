cd3cb95962bebce812f46464bd39c84c183e84a0
Adithya311203 <97468387+Adithya311203@users.noreply.github.com>
2024-02-11 23:47:14 +0530

    Create threefr.py

---
+++ threefr.py
@@ -0,0 +1,14 @@
+# cook your dish here
+t=int(input())
+for _ in range(t):
+    x,y,z=map(int,input().split())
+    l=[x,y,z]
+    l.sort()
+    x,y,z=l[2],l[1],l[0]
+    a=x+y-z
+    b=x-y+z
+    c=x-y-z
+    if a==0 or b==0 or c == 0:
+        print("yes")
+    else:
+        print("no")
